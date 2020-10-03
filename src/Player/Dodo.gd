bbb20dbc183b96d2f631f0df4a88a51ef3d89f95
Megepok <yvann.gouraud@hotmail.fr>
2020-10-03 14:26:50 +0200

    add sprites

--- src/Player/Dodo.gd
+++ src/Player/Dodo.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 signal sp
 
-var grav = 400
+var grav = 0
 var TAILLE_ECRANT2 = get_viewport_rect().size
 var speed_player = 400
 var nbsp = 3 #Le nombre de sp va de 0 à 3
@@ -10,6 +10,7 @@ var nbsp = 3 #Le nombre de sp va de 0 à 3
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.animation = "1walk"
+	position = TAILLE_ECRANT2
 	pass # Replace with function body.
 
 func sp_bar(): #Gère la barre de SP
@@ -20,17 +21,19 @@ func sp_bar(): #Gère la barre de SP
 func _process(delta):
 	var velocity = Vector2.ZERO #Mouvement du joueur en 2D 
 	
-	velocity.y = grav
-	velocity = velocity * speed_player
+	#velocity.y = grav
+	#velocity = velocity * speed_player
 	if Input.is_action_pressed("ui_select"):
-		velocity.y -= 100
+		velocity.y -= 1
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 100
+		velocity.x += 1
 	#if velocity.y < 0: #Lors d'un saut, on passe au sprite "jump"
 		#$AnimatedSprite.animation = "jump"
 	#if velocity.y > 0: #Lors d'une déscente, on passe au sprite "drop"
 		#$AnimatedSprite.animation = "drop"
-	position += velocity*delta
+	position += velocity*speed_player
 	position.x = clamp(position.x, 0, TAILLE_ECRANT2.x) #Limitation du déplacement sur l'écran
 	position.y = clamp(position.y, 0, TAILLE_ECRANT2.y)
+
+#func jump():
 	
