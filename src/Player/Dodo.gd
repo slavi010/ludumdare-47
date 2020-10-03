ff7907deb504938d6b6a129c276a1702c4bc205c
Megepok <yvann.gouraud@hotmail.fr>
2020-10-03 13:19:55 +0200

    ajout du sprite Dodo ajout de sprite Placeholder

--- src/Player/Dodo.gd
+++ src/Player/Dodo.gd
@@ -2,12 +2,14 @@ extends Area2D
 
 signal sp
 
-
-
+var grav = 400
+var TAILLE_ECRANT2 = get_viewport_rect().size
+var speed_player = 400
 var nbsp = 3 #Le nombre de sp va de 0 à 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimatedSprite.animation = "1walk"
 	pass # Replace with function body.
 
 func sp_bar(): #Gère la barre de SP
@@ -16,16 +18,19 @@ func sp_bar(): #Gère la barre de SP
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2() #Mouvement du joueur en 2D 
+	var velocity = Vector2.ZERO #Mouvement du joueur en 2D 
 	
-	#var gravity = -10 
-	#velocity.y += gravity*delta
-	
+	velocity.y = grav
+	velocity = velocity * speed_player
 	if Input.is_action_pressed("ui_select"):
-		velocity.y -= 1
-	if velocity.y < 0: #Lors d'un saut, on passe au sprite "jump"
-		$AnimatedSprite.animation = "jump"
-	if velocity.y > 0: #Lors d'une déscente, on passe au sprite "drop"
-		$AnimatedSprite.animation = "drop"
-	
+		velocity.y -= 100
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 100
+	#if velocity.y < 0: #Lors d'un saut, on passe au sprite "jump"
+		#$AnimatedSprite.animation = "jump"
+	#if velocity.y > 0: #Lors d'une déscente, on passe au sprite "drop"
+		#$AnimatedSprite.animation = "drop"
+	position += velocity*delta
+	position.x = clamp(position.x, 0, TAILLE_ECRANT2.x) #Limitation du déplacement sur l'écran
+	position.y = clamp(position.y, 0, TAILLE_ECRANT2.y)
 	
