48e1cbd21f90766321d52c6d0f806fd213010ad6
Megepok <yvann.gouraud@hotmail.fr>
2020-10-03 03:46:11 +0200

    modified Main.gd

--- src/Main.gd
+++ src/Main.gd
@@ -2,9 +2,18 @@ extends Node
 
 signal beat
 
+export var largeur = 5 #nombre de block en largeur
+export var decalage = 1 #décalage du joueur 
+
 func _on_Rythme_timeout(): #A chaque beat envoi un signal
 	emit_signal("beat")
 
+func new_place():
+	 #if (): #Condition correspondant au lieu où se trouve le joueur
+		#$Rythme.wait_time(n) Changement du temps entre chaque beat
+	#else:
+		#Rythme.wait_time(n/2)
+	pass
 
 
 # Called when the node enters the scene tree for the first time.
