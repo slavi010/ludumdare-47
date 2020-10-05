83db44c5c21a2ed9f70d0302da7f4c107b4ea540
Aurelien Boissiere <aurelien.boissiere@etu.univ-nantes.fr>
2020-10-05 22:59:46 +0200

    Crédits finaux

---
+++ src/UI/Menu/CreditsButtons.gd
@@ -0,0 +1,16 @@
+extends Label
+
+var is_mouse_in : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed() and is_mouse_in:
+		$"../../Crédits".show()
+
+
+func _on_CreditsButtons_mouse_entered():
+	is_mouse_in = true
+
+
+func _on_CreditsButtons_mouse_exited():
+	is_mouse_in = false
